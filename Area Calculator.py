2aa784219c8d035e4e56f2a4435e5e8257565f45
Den-Freeman <52362406+Den-Freeman@users.noreply.github.com>
2022-02-01 00:32:18 +0200

    Updated - added option for TRIANGE
    Added TRIANGLE area calculator option

--- Area Calculator.py
+++ Area Calculator.py
@@ -9,3 +9,15 @@ if option == 'C':
   radius = float(input("Enter radius: "))
   area = 3.14159 * (radius**2)
   print ("Area: %f" % area)
+  
+elif option == 'T':
+  base = float(input("Enter base: "))
+  height = float(input("Enter height: "))
+  area = 0.5 * base * height
+  print ("Area: %f" % area)
+
+else:
+  print ("Error! Invalid shape.")
+
+
+print ("Exiting...")
